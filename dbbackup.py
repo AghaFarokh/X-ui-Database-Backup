870a95ba4c7e14106c21392c5656b0b5f3117cc1
AghaFarokh <34651966+AghaFarokh@users.noreply.github.com>
2023-03-11 07:44:02 +0330

    Updated whole file

--- dbbackup.py
+++ dbbackup.py
@@ -1,27 +1,29 @@
 import os
-import socket
 import time
 import telebot
 
-TOKEN = '<your_bot_token>'
-CHAT_ID = '<your_chat_id>'
+# Set up the bot
+bot = telebot.TeleBot("YOUR_API_TOKEN")
 
-bot = telebot.TeleBot(TOKEN)
+# Set the chat ID to send the file to
+chat_id = "YOUR_CHAT_ID"
+
+# Get the hostname and IP address of the current server
+hostname = os.uname().nodename
+ip_address = os.popen("curl -s https://api.ipify.org").read().strip()
+
+# Set the path to the database file
+db_path = "/etc/x-ui/x-ui.db"
+
+# Set the interval to send the file (in seconds)
+interval = 6 * 60 * 60
 
 while True:
-    # Get server hostname and IP address
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    
-    # Construct the message
-    message = f'Database file from {hostname} ({ip_address})\n'
-    
-    # Read the database file
-    with open('/etc/x-ui/x-ui.db', 'rb') as file:
-        contents = file.read()
-    
-    # Send the message and file
-    bot.send_document(CHAT_ID, contents, caption=message)
-    
-    # Wait for 6 hours
-    time.sleep(6 * 60 * 60)
+    # Sleep for the specified interval
+    time.sleep(interval)
+
+    # Compose the message with the server hostname, IP address, and database file
+    message = f"Server: {hostname} ({ip_address})\nDatabase file:"
+    with open(db_path, "rb") as file:
+        bot.send_document(chat_id, file, caption=message)
+
